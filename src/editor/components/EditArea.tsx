42ee5ecde5496031ab53baa76f2b3852b55aca74
kurobakaito <2136267433@qq.com>
2025-01-14 17:19:54 +0800

    feat: add components stores

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,3 +1,34 @@
+import { useEffect } from "react";
+import { useComponentsStore } from "../stores/components";
+
 export function EditArea() {
-    return <div>EditArea</div>
+    const { components, addComponent, deleteComponent } = useComponentsStore();
+
+    useEffect(() => {
+        addComponent({
+            id: 222,
+            name: 'Container',
+            props: {},
+            children: []
+        }, 1);
+
+        addComponent({
+            id: 333,
+            name: 'Video',
+            props: {},
+            children: []
+        }, 222);
+
+        setTimeout(() => {
+            deleteComponent(333)
+        }, 3000);
+    }, [])
+
+    return <div>
+        <pre>
+            {
+                JSON.stringify(components, null, 2)
+            }
+        </pre>
+    </div>
 }
