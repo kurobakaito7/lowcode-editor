5fdd57dde69dc96e5f1fbe57647012ebc95ec60e
kurobakaito <kurobakaito77777@gmail.com>
2025-03-18 19:14:51 +0800

    deploy test 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
